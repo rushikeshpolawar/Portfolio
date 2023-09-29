9ccb77e49ee50412f4f3858b0b372306d877e474
rushikesh <rushikeshp@vconstruct.in>
2023-09-30 00:33:05 +0530

    toast message

--- rushikesh_portfolio/src/App.tsx
+++ rushikesh_portfolio/src/App.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import {
   About,
   Contact,
